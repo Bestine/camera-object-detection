b5e0bd09793afdadb2a04026054b747832bb7173
Bestine <bestineokinda@gmail.com>
2023-10-05 22:00:36 +0300

    New class to open camera

--- src/main/java/com/the_ape/application/Application.java
+++ src/main/java/com/the_ape/application/Application.java
@@ -1,7 +1,12 @@
 package com.the_ape.application;
 
+import com.the_ape.application.CameraSnapshotJavaFX;
+
 public class Application {
+    static CameraSnapshotJavaFX cameraframe;
     public static void main(String []args){
         System.out.println("Hello Mustafa");
+        cameraframe = new CameraSnapshotJavaFX();
+        cameraframe.open_camera();
     }
 }
---
+++ src/main/java/com/the_ape/application/CameraSnapshotJavaFX.java
@@ -0,0 +1,7 @@
+package com.the_ape.application;
+
+public class CameraSnapshotJavaFX {
+    public static void open_camera(){
+        System.out.println("Opening the camera");
+    }
+}
