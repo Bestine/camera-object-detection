608141cf8d7628bb83aea65b6f2c2555571784d6
Bestine <bestineokinda@gmail.com>
2023-10-07 15:43:21 +0300

    Started over the application with JFrame

--- src/main/java/com/the_ape/application/CameraViewer.java
+++ src/main/java/com/the_ape/application/CameraViewer.java
@@ -39,79 +39,81 @@ package com.the_ape.application;
 //    }
 //}
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.videoio.VideoCapture;
-import org.opencv.videoio.Videoio;
 
-public class CameraViewer extends Application {
 
-    static {
-        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-    }
-
-    private VideoCapture capture;
-    private ImageView imageView;
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) {
-        StackPane root = new StackPane();
-        imageView = new ImageView();
-        root.getChildren().add(imageView);
-
-        Scene scene = new Scene(root, 640, 480);
-
-        primaryStage.setTitle("Camera Viewer");
-        primaryStage.setScene(scene);
-        primaryStage.setOnCloseRequest(e -> {
-            stopCamera();
-            System.exit(0);
-        });
-        primaryStage.show();
-
-        startCamera();
-    }
-
-    private void startCamera() {
-        capture = new VideoCapture(0); // 0 represents the default camera
-        if (capture.isOpened()) {
-            capture.set(Videoio.CAP_PROP_FRAME_WIDTH, 640);
-            capture.set(Videoio.CAP_PROP_FRAME_HEIGHT, 480);
-
-            Runnable frameGrabber = () -> {
-                Mat frame = new Mat();
-                if (capture.read(frame)) {
-                    Image img = Utils.mat2Image(frame);
-                    updateImageView(imageView, img);
-                }
-            };
-
-            new Thread(() -> {
-                while (true) {
-                    frameGrabber.run();
-                }
-            }).start();
-        }
-    }
-
-    private void stopCamera() {
-        if (capture != null && capture.isOpened()) {
-            capture.release();
-        }
-    }
-
-    private void updateImageView(ImageView view, Image image) {
-        // Run this on the JavaFX application thread
-        javafx.application.Platform.runLater(() -> view.setImage(image));
-    }
-}
+//import javafx.application.Application;
+//import javafx.scene.Scene;
+//import javafx.scene.image.Image;
+//import javafx.scene.image.ImageView;
+//import javafx.scene.layout.StackPane;
+//import javafx.stage.Stage;
+//import org.opencv.core.Core;
+//import org.opencv.core.Mat;
+//import org.opencv.videoio.VideoCapture;
+//import org.opencv.videoio.Videoio;
+//
+//public class CameraViewer extends Application {
+//
+//    static {
+//        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+//    }
+//
+//    private VideoCapture capture;
+//    private ImageView imageView;
+//
+//    public static void main(String[] args) {
+//        launch(args);
+//    }
+//
+//    @Override
+//    public void start(Stage primaryStage) {
+//        StackPane root = new StackPane();
+//        imageView = new ImageView();
+//        root.getChildren().add(imageView);
+//
+//        Scene scene = new Scene(root, 640, 480);
+//
+//        primaryStage.setTitle("Camera Viewer");
+//        primaryStage.setScene(scene);
+//        primaryStage.setOnCloseRequest(e -> {
+//            stopCamera();
+//            System.exit(0);
+//        });
+//        primaryStage.show();
+//
+//        startCamera();
+//    }
+//
+//    private void startCamera() {
+//        capture = new VideoCapture(0); // 0 represents the default camera
+//        if (capture.isOpened()) {
+//            capture.set(Videoio.CAP_PROP_FRAME_WIDTH, 640);
+//            capture.set(Videoio.CAP_PROP_FRAME_HEIGHT, 480);
+//
+//            Runnable frameGrabber = () -> {
+//                Mat frame = new Mat();
+//                if (capture.read(frame)) {
+//                    Image img = Utils.mat2Image(frame);
+//                    updateImageView(imageView, img);
+//                }
+//            };
+//
+//            new Thread(() -> {
+//                while (true) {
+//                    frameGrabber.run();
+//                }
+//            }).start();
+//        }
+//    }
+//
+//    private void stopCamera() {
+//        if (capture != null && capture.isOpened()) {
+//            capture.release();
+//        }
+//    }
+//
+//    private void updateImageView(ImageView view, Image image) {
+//        // Run this on the JavaFX application thread
+//        javafx.application.Platform.runLater(() -> view.setImage(image));
+//    }
+//}
---
+++ src/main/java/com/the_ape/application/WebcamCapture.java
@@ -0,0 +1,7 @@
+//package com.the_ape.application;
+//
+//public class WebcamCapture {
+//    public static void main(String[] args){
+//
+//    }
+//}
