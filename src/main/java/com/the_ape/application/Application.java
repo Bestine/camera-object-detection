1480f10f87e161ecb26aa2efb2b6b2e8660bcbd9
Bestine <bestineokinda@gmail.com>
2023-10-12 12:09:44 +0300

    Opened Webcam with sarxos webcam capture library

--- src/main/java/com/the_ape/application/Application.java
+++ src/main/java/com/the_ape/application/Application.java
@@ -1,15 +1,30 @@
 package com.the_ape.application;
 
 
+import com.github.sarxos.webcam.Webcam;
 import org.opencv.core.Core;
 
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+
 public class Application{
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Started over the application");
         //previously
         //-Djava.library.path=/home/ustadhi/java_libraries/opencv/build/java/opencv-480.jar
         //System.loadLibrary(Core.NATIVE_LIBRARY_NAME); //loads native library of opencv
-        System.loadLibrary("opencv_java480.dll");
-        new CameraView();
+
+//        System.loadLibrary("opencv_java480.dll");
+//        new CameraView();
+
+
+        /* Try with Webcam capture tutorial */
+        Webcam webcam = Webcam.getDefault();
+        webcam.open();
+        ImageIO.write(webcam.getImage(),
+                "JPG",
+                new File("first_capture.jpg"));
+        
     }
 }
